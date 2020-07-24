a01f9ee7421708314f1bec078189cecc13d403ea
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2020-07-24 18:17:53 +0530

    Update users.js
    Salt added for hashing

--- users.js
+++ users.js
@@ -1,3 +1,7 @@
+const jwt=require('jsonwebtoken');
+const bcrypt=require('bcrypt');
+const confiq=require('../config/config').get(process.env.NODE_ENV);
+const salt=10;
 const express=require('express');
 const mongoose= require('mongoose');
 const bodyparser=require('body-parser');
