45c0826df36200772478d7fe7de94218b2b04df4
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2020-07-24 18:23:29 +0530

    Create login.js
    generates token to login

---
+++ login.js
@@ -0,0 +1,10 @@
+userSchema.methods.generateToken=function(cb){
+    var user =this;
+    var token=jwt.sign(user._id.toHexString(),confiq.SECRET);
+
+    user.token=token;
+    user.save(function(err,user){
+        if(err) return cb(err);
+        cb(null,user);
+    })
+}
