73c6d5edc4610b236d421736e98899dd71f1126d
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2020-07-24 18:24:52 +0530

    Create find_user.js
    used to find whether user is logged in or not

---
+++ find_user.js
@@ -0,0 +1,10 @@
+userSchema.statics.findByToken=function(token,cb){
+    var user=this;
+
+    jwt.verify(token,confiq.SECRET,function(err,decode){
+        user.findOne({"_id": decode, "token":token},function(err,user){
+            if(err) return cb(err);
+            cb(null,user);
+        })
+    })
+};
