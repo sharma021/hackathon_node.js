4e0ee411ffbb8e8e0ed508cdf68faaa9c974cb39
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2020-07-24 18:09:11 +0530

    Update users.js
    Added database to users.js

--- users.js
+++ users.js
@@ -1,18 +1,30 @@
-const express= require('express');
+const express=require('express');
 const mongoose= require('mongoose');
-const bodyparser= require('body-parser');
-const cookieparser= require('cookie-parser');
+const bodyparser=require('body-parser');
+const cookieParser=require('cookie-parser');
+const db=require('./config/config').get(process.env.NODE_ENV);
 
-const app=express()
-app.use(bodyparser.urlencoded({extended:false}));
+
+const app=express();
+
+app.use(bodyparser.urlencoded({extended : false}));
 app.use(bodyparser.json());
-app.use(cookieparser());
+app.use(cookieParser());
 
-app.get('/',function(req,res){
-    res.status(200).send('Welcome to Sign Up API');
+
+mongoose.Promise=global.Promise;
+mongoose.connect(db.DATABASE,{ useNewUrlParser: true,useUnifiedTopology:true },function(err){
+    if(err) console.log(err);
+    console.log("database is connected");
 });
 
-const PORT = process.env.PORT||3000;
-app.listen(PORT,()=> {
-    console.log(app is live at ${PORT});
+
+app.get('/',function(req,res){
+    res.status(200).send(`Welcome to login , sign-up api`);
+});
+
+
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>{
+    console.log(`app is live at ${PORT}`);
 });
