b5bb315808e667e1fae17f59172b8b7dd53a1b5a
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2020-07-24 18:26:14 +0530

    Create delete.js
    user is deleted after log out

---
+++ delete.js
@@ -0,0 +1,9 @@
+
+userSchema.methods.deleteToken=function(token,cb){
+    var user=this;
+
+    user.update({$unset : {token :1}},function(err,user){
+        if(err) return cb(err);
+        cb(null,user);
+    })
+}
