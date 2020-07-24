e0f2ff1428e17742dbc4d3ca18f4b864c3404447
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2020-07-24 18:22:07 +0530

    Create compare.js
    Checked whether passwords are same or not

---
+++ compare.js
@@ -0,0 +1,6 @@
+userSchema.methods.comparepassword=function(password,cb){
+    bcrypt.compare(password,this.password,function(err,isMatch){
+        if(err) return cb(next);
+        cb(null,isMatch);
+    });
+}
