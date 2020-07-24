3da938022f4f92fa21d149433380895a7d222fe9
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2020-07-24 18:27:29 +0530

    Update users.js

--- users.js
+++ users.js
@@ -32,3 +32,23 @@ const PORT=process.env.PORT||3000;
 app.listen(PORT,()=>{
     console.log(`app is live at ${PORT}`);
 });
+
+app.post('/api/register',function(req,res){
+   // taking a user
+   const newuser=new User(req.body);
+   
+  if(newuser.password!=newuser.password2)return res.status(400).json({message: "password not match"});
+   
+   User.findOne({email:newuser.email},function(err,user){
+       if(user) return res.status(400).json({ auth : false, message :"email exits"});
+
+       newuser.save((err,doc)=>{
+           if(err) {console.log(err);
+               return res.status(400).json({ success : false});}
+           res.status(200).json({
+               succes:true,
+               user : doc
+           });
+       });
+   });
+});
