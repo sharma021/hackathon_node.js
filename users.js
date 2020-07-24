c89a9c4b658fbd6eb5225404359a7a80d56ad7c2
Vidya Sharma <63513761+sharma021@users.noreply.github.com>
2020-07-24 17:56:49 +0530

    Update users.js

--- users.js
+++ users.js
@@ -1,19 +1,18 @@
-const express = require("express");
-const router = express.Router();
-const mongoose = require("mongoose");
+const express= require('express');
+const mongoose= require('mongoose');
+const bodyparser= require('body-parser');
+const cookieparser= require('cookie-parser');
 
-const User = require("../models/user");
+const app=express()
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+app.use(cookieparser());
 
-router.post("/signup", (req,res,next) => {
-    const user = new user({
-        _id: new mongoose.Types.ObjectId(),
-        Index_No: req.body.Index_No,
-        Name: req.body.Name,
-        Email: req.body.Email,
-        Contact: req.body.Contact,
-        date_created: req.body.date_created,
-        list: req.body.list,
-        Budget: req.body.Budget
-    });
+app.get('/',function(req,res){
+    res.status(200).send('Welcome to Sign Up API');
 });
-module.exports = router;
+
+const PORT = process.env.PORT||3000;
+app.listen(PORT,()=> {
+    console.log(app is live at ${PORT});
+});
